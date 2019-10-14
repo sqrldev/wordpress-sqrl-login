eb3456023a496d7c7551d67219d614678a0a1df0
Daniel Persson <mailto.woden@gmail.com>
2019-10-14 10:20:17 +0200

    Typo

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -12,31 +12,31 @@ class PluginTest extends WP_UnitTestCase {
 
     $_SERVER['HTTPS'] = 0;
     $message = $sqrlLogin->user_login_message();
-    $this->assertEqual( "SQRL Login is only available for sites utilizing SSL connections. Please activate SSL before using SQRL Login.", $message );
+    $this->assertEquals( "SQRL Login is only available for sites utilizing SSL connections. Please activate SSL before using SQRL Login.", $message );
 
     $_SERVER['HTTPS'] = 1;
     $message = $sqrlLogin->user_login_message(); 
-    $this->assertEqual( "", $message );
+    $this->assertEquals( "", $message );
 
     $_GET['message'] = SQRLLogin::MESSAGE_DISABLED;
     $message = $sqrlLogin->user_login_message();
-    $this->assertEqual( "Account disabled", $message );
+    $this->assertEquals( "Account disabled", $message );
 
     $_GET['message'] = SQRLLogin::MESSAGE_REMOVED;
     $message = $sqrlLogin->user_login_message();
-    $this->assertEqual( "Identity disassociated from account", $message );
+    $this->assertEquals( "Identity disassociated from account", $message );
 
     $_GET['message'] = SQRLLogin::MESSAGE_SQRLONLY;
     $message = $sqrlLogin->user_login_message();
-    $this->assertEqual( "The only allowed login method is SQRL for this account", $message );
+    $this->assertEquals( "The only allowed login method is SQRL for this account", $message );
 
     $_GET['message'] = SQRLLogin::MESSAGE_ERROR;
     $message = $sqrlLogin->user_login_message();
-    $this->assertEqual( "An error occured with the last SQRL command, please try again.", $message );
+    $this->assertEquals( "An error occured with the last SQRL command, please try again.", $message );
 
     $_GET['message'] = SQRLLogin::MESSAGE_REGISTRATION_NOT_ALLOWED;
     $message = $sqrlLogin->user_login_message();
-    $this->assertEqual( "The site is not allowing new registrations and your SQRL identity is not associated with any account.", $message );
+    $this->assertEquals( "The site is not allowing new registrations and your SQRL identity is not associated with any account.", $message );
   }
 }
 
