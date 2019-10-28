2fe374d30cd15eaa93d0504a9566e505c94eda24
Daniel Persson <mailto.woden@gmail.com>
2019-10-28 21:29:13 +0100

    Test

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -449,7 +449,7 @@ class PluginTest extends WP_UnitTestCase {
 
     $_POST["ids"] = $this->base64url_encode($signature);
 
-    $response = runAndReturn($sqrlLogin->api_callback());
+    $response = $this->runAndReturn($sqrlLogin->api_callback());
     var_dump($response);
 
     $session = get_transient($response["nut"]);
